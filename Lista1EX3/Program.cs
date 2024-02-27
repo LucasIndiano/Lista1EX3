cd1fede60e02d705c934445f33f190388cc6e581
ALUNO <ALUNO@LABHPC22>
2024-02-27 16:01:30 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            Console.WriteLine("Digite seu sexo (M para Masculino e F para Feminino): ");
+            sexo = char.Parse(Console.ReadLine());
+            if (sexo == 'M' || sexo == 'm' || sexo == 'F' || sexo == 'f')
+            {
+                Console.WriteLine("Sexo Válido.");
+            } 
+        else { Console.WriteLine("Sexo Inválido."); }
+
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
